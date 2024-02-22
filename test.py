3028c5c0d668d5030ced1b6a103905eaf8e95318
butlerdtc <butlerr2@middleton.school.nz>
2024-02-23 11:02:37 +1300

    Initial commit

---
+++ test.py
@@ -0,0 +1,28 @@
+import easygui
+import random
+
+number = random.randint(0, 5)
+print(number)
+
+words = ["bat", "cat", "hat", "mat"]
+my_word = random.choice(words)
+print(my_word)
+
+friend = "Rafael"
+letter = random.choice(friend)
+print(letter)
+
+welcome = easygui.msgbox("Welcome to Easygui", "Welcome")
+
+name = easygui.enterbox("Hi, Whats your name? ", "Name")
+print(name)
+
+age = easygui.integerbox("How old are you? ")
+print(age)
+
+last_name = easygui.buttonbox("Choose gender", "Gender",
+                              ["Male", "Female", "Other"])
+
+carry_on = easygui.buttonbox("Do you want to continue?",
+                             choices=["Yes", "No", "Don't care"],
+                             title="Continue")
