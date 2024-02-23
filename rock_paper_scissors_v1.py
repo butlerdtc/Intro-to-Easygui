2b0db9ea81d4fd34fcfddbc77071723d19cc6e15
butlerdtc <butlerr2@middleton.school.nz>
2024-02-23 16:40:12 +1300

    Create rock_paper_scissors_v1.py

---
+++ rock_paper_scissors_v1.py
@@ -0,0 +1,7 @@
+import easygui
+import random
+
+welcome = easygui.buttonbox("Welcome to ,Rock Paper, Scissors\n Rock beats "
+                         "Scissors\n Scissors beats Paper\n Paper beats Rock\n"
+                           "Do you want to play? ", "Welcome and Rules",
+                            ["Yes", "No"])
