d952df1477ed5f3d3ed0e682d760c2708c741cf7
butlerdtc <butlerr2@middleton.school.nz>
2024-03-06 13:50:50 +1300

    Update

---
+++ Working with Lists/1_spelling_converter_v1.py
@@ -0,0 +1,14 @@
+"""Convert NZ English Spelling to US English - Spelling Converter v1"""
+
+import easygui
+
+word = easygui.enterbox("Please enter word to convert", "User Word")
+# Convert user input to a list
+word_list = list(word)
+letters_to_remove = "u"
+position = (word.find(letters_to_remove))
+for letter in range(len(letters_to_remove)):
+    word_list.pop(position)
+word_list = ''.join(word_list)
+output = easygui.msgbox(f"Your word, {word} in American spelling is "
+                        f"{word_list}")
