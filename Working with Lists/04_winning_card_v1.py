9d4c90319b4ef5ab4aa414d38dd162a2fb2549a7
butlerdtc <butlerr2@middleton.school.nz>
2024-03-07 10:28:10 +1300

    Update 04_winning_card_v1.py

--- Working with Lists/04_winning_card_v1.py
+++ Working with Lists/04_winning_card_v1.py
@@ -1,3 +1,8 @@
 """Winning card game v1"""
 
 import easygui
+import random
+
+cards = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+         "Ten", "Jack", "Queen", "King", "Ace"]
+suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
