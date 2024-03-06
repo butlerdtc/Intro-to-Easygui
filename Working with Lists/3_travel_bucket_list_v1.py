9e9400fe56f38c03aa025c5009365bbc85bf9d80
butlerdtc <butlerr2@middleton.school.nz>
2024-03-06 17:07:13 +1300

    Update 3_travel_bucket_list_v1.py

--- Working with Lists/3_travel_bucket_list_v1.py
+++ Working with Lists/3_travel_bucket_list_v1.py
@@ -1,5 +1,19 @@
-"""Travel Bucket list """
+"""Travel Bucket list v1 """
 
 import easygui
+
 while True:
-    places = easygui.msgbox(msg="monkey")
+    places = easygui.enterbox("What are your 5 bucket list places you would "
+                              "like to visit\nSeparate each place with a "
+                              "comma", "Enter favourite places")
+    number_places = places.split(",")
+    if len(number_places) != 5:
+        easygui.msgbox(f"Please enter 5 places\nYou entered "
+                       f"{len(number_places)} place", "Error")
+        continue
+    else:
+        break
+
+result = "\n-    ".join(number_places)
+easygui.msgbox(f"My bucket list places: \n\n-    {result}", "Travel "
+                                                            "Bucket List")
