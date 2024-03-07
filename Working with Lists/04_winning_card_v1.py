ecb5e878da722e3e4b8ce067d5598892d38834dd
butlerdtc <butlerr2@middleton.school.nz>
2024-03-07 17:37:49 +1300

    Update 04_winning_card_v1.py

--- Working with Lists/04_winning_card_v1.py
+++ Working with Lists/04_winning_card_v1.py
@@ -3,35 +3,51 @@
 import easygui
 import random
 
-computer = []
-player = []
-number = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-          "Ten", "Jack", "Queen", "King", "Ace"]
-suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
+while True:
+    intro = easygui.buttonbox("Would you like to play?", "Intro "
+                              "Screen", ["Yes", "No"])
+    if intro == "Yes":
+        computer = []
+        player = []
+        number = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+                  "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+        suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
 
-# Generate random combination for computer
-computer_number = random.choice(number)
-computer_suit = random.choice(suits)
-computer_value = number.index(computer_number)
+        # Generate random combination for computer
+        computer_number = random.choice(number)
+        computer_suit = random.choice(suits)
+        computer_value = number.index(computer_number)
 
-# Removes computer number from list
-number.pop(computer_value)
+        # While loop to check computer and player don't have same card and fix
+        while True:
+            # Generate random combination for player
+            player_number = random.choice(number)
+            player_suit = random.choice(suits)
+            player_value = number.index(player_number)
+            if computer_value != player_value or computer_suit != player_suit:
+                break
+            else:
+                continue
 
-# Generate random combination for player
-player_number = random.choice(number)
-player_suit = random.choice(suits)
+        # Adds computer and player card to a list
+        computer.append(computer_number)
+        computer.append(computer_suit)
+        player.append(player_number)
+        player.append(player_suit)
 
-# Adds computer and player card to a list
-computer.append(computer_number)
-computer.append(computer_suit)
-player.append(player_number)
-player.append(player_suit)
+        # Assigns player number an index
+        player_result = number.index(player_number)
 
-player_result = number.index(player_number)
+        easygui.msgbox(f"The Computer had the {computer[0]} of {computer[1]} "
+                       f"and you had the {player[0]} of {player[1]}")
 
-easygui.msgbox(f"The Computer had the {computer[0]} of {computer[1]} and "
-               f"you had the {player[0]} of {player[1]}")
-if computer_value > player_result:
-    easygui.msgbox("The computer won")
-else:
-    easygui.msgbox("You won")
+        if computer_value == player_result:
+            easygui.msgbox("It was a draw")
+        elif computer_value > player_result:
+            easygui.msgbox("The computer won")
+        else:
+            easygui.msgbox("You won")
+    else:
+        break
+
+easygui.msgbox("Thanks for playing\n\n         Goodbye", "Goodbye")
