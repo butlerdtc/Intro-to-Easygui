ac45e9b30bd49a6a9c6019518d499efba53e4ac6
butlerdtc <butlerr2@middleton.school.nz>
2024-03-12 10:24:01 +1300

    Update

--- Working with Lists/06_travel_bucket_list_v2.py
+++ Working with Lists/06_travel_bucket_list_v2.py
@@ -17,3 +17,14 @@ while True:
 result = "\n-    ".join(number_places)
 easygui.msgbox(f"My bucket list places: \n\n-    {result}", "Travel "
                                                             "Bucket List")
+
+new_places = easygui.enterbox("What place do you want to change?\nSeparate "
+                              "with a comma")
+
+change_place, new_place = new_places.split(",")
+replacement_index = number_places.index(change_place.strip())
+number_places[replacement_index] = new_place.strip()
+
+second_result = "\n-    ".join(number_places)
+easygui.msgbox(f"My bucket list places: \n\n-    {second_result}",
+               "Travel Bucket List")
--- test.py
+++ test.py
@@ -4,25 +4,31 @@ import random
 number = random.randint(0, 5)
 print(number)
 
-words = ["bat", "cat", "hat", "mat"]
-my_word = random.choice(words)
-print(my_word)
+words = ["bat", "cat", "sat", "pat"]
+# my_word = random.choice(words)
+# print(my_word)
+#
+# friend = "Rafael"
+# letter = random.choice(friend)
+# print(letter)
+#
+# welcome = easygui.msgbox("Welcome to Easygui", "Welcome")
+#
+# name = easygui.enterbox("Hi, Whats your name? ", "Name")
+# print(name)
+#
+# age = easygui.integerbox("How old are you? ")
+# print(age)
+#
+# last_name = easygui.buttonbox("Choose gender", "Gender",
+#                               ["Male", "Female", "Other"])
+#
+# carry_on = easygui.buttonbox("Do you want to continue?",
+#                              choices=["Yes", "No", "Don't care"],
+#                              title="Continue")
+list_1 = ["Monkey"]
+list_2 = list_1[-1::2]
+print(list_2)
 
-friend = "Rafael"
-letter = random.choice(friend)
-print(letter)
-
-welcome = easygui.msgbox("Welcome to Easygui", "Welcome")
-
-name = easygui.enterbox("Hi, Whats your name? ", "Name")
-print(name)
-
-age = easygui.integerbox("How old are you? ")
-print(age)
-
-last_name = easygui.buttonbox("Choose gender", "Gender",
-                              ["Male", "Female", "Other"])
-
-carry_on = easygui.buttonbox("Do you want to continue?",
-                             choices=["Yes", "No", "Don't care"],
-                             title="Continue")
+words.sort(reverse=True)
+print(words)
