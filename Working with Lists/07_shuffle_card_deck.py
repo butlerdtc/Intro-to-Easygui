5e0544c80038684b787758e859c4e0d6572a096d
butlerdtc <butlerr2@middleton.school.nz>
2024-03-13 09:34:01 +1300

    Create 07_shuffle_card_deck.py

---
+++ Working with Lists/07_shuffle_card_deck.py
@@ -0,0 +1,9 @@
+"""Card deck shuffler v1"""
+
+import easygui
+import random
+
+number = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+          "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
+        
