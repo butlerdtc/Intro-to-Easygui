32cc730f15ed51d8a34c6dc2e1c1d0b4324ee2e8
butlerdtc <butlerr2@middleton.school.nz>
2024-03-12 13:33:11 +1300

    Update test.py

--- test.py
+++ test.py
@@ -5,27 +5,27 @@ number = random.randint(0, 5)
 print(number)
 
 words = ["bat", "cat", "sat", "pat"]
-# my_word = random.choice(words)
-# print(my_word)
-#
-# friend = "Rafael"
-# letter = random.choice(friend)
-# print(letter)
-#
-# welcome = easygui.msgbox("Welcome to Easygui", "Welcome")
-#
-# name = easygui.enterbox("Hi, Whats your name? ", "Name")
-# print(name)
-#
-# age = easygui.integerbox("How old are you? ")
-# print(age)
-#
-# last_name = easygui.buttonbox("Choose gender", "Gender",
-#                               ["Male", "Female", "Other"])
-#
-# carry_on = easygui.buttonbox("Do you want to continue?",
-#                              choices=["Yes", "No", "Don't care"],
-#                              title="Continue")
+my_word = random.choice(words)
+print(my_word)
+
+friend = "Rafael"
+letter = random.choice(friend)
+print(letter)
+
+welcome = easygui.msgbox("Welcome to Easygui", "Welcome")
+
+name = easygui.enterbox("Hi, Whats your name? ", "Name")
+print(name)
+
+age = easygui.integerbox("How old are you? ")
+print(age)
+
+last_name = easygui.buttonbox("Choose gender", "Gender",
+                              ["Male", "Female", "Other"])
+
+carry_on = easygui.buttonbox("Do you want to continue?",
+                             choices=["Yes", "No", "Don't care"],
+                             title="Continue")
 list_1 = ["Monkey"]
 list_2 = list_1[-1::2]
 print(list_2)
