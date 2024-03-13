021d7e7a24acfc78bb37099635901e237a4c9fb5
butlerdtc <butlerr2@middleton.school.nz>
2024-03-13 19:25:55 +1300

    Update 08_yahtzee_lite_v1.py

--- Working with Lists/08_yahtzee_lite_v1.py
+++ Working with Lists/08_yahtzee_lite_v1.py
@@ -6,19 +6,39 @@ import random
 dice = ["1", "2", "3", "4", "5", "6"]
 number = 0
 while True:
-    result = []
+    results = []
     for i in range(5):
         rolled = random.choice(dice)
-        result.append(rolled)
-    result.sort()
-    final_result = ", ".join(result)
+        results.append(rolled)
+    results.sort()
+    final_result = ", ".join(results)
     sequence = easygui.buttonbox(f"You have rolled: {final_result}\n\n"
                                  f"Choose:", "Result",
                                  ["Roll again", "Stick"])
     if sequence == "Stick" or number == 2:
-        for item in result:
-            if item == result[item]:
-                
+        three_kind = any(results.count(result) >= 3 for result in results)
+        four_kind = any(results.count(result) >= 4 for result in results)
+        yahtzee = any(results.count(result) == 5 for result in results)
+
+        if yahtzee:
+            easygui.msgbox(f"You rolled: {final_result}\n\nYahtzee!",
+                           "Winner")
+        elif four_kind:
+            easygui.msgbox(f"You rolled: {final_result}\n\nFour of a "
+                           f"kind!", "Winner")
+        elif three_kind:
+            easygui.msgbox(f"You rolled: {final_result}\n\nThree of a kind!",
+                           "Winner")
+        else:
+            easygui.msgbox(f"You rolled: {final_result}\n\nBetter"
+                           f" luck next time", "Loser")
+
+        goodbye = easygui.buttonbox("Would you like to play another round?",
+                                    "Replay", ["Yes", "No"])
+        if goodbye == "Yes":
+            continue
+        else:
+            break
     else:
         number += 1
         continue
