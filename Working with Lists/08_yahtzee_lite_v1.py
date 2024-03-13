cb2be3b07b524e54340370319f16e66cee4e9ded
butlerdtc <butlerr2@middleton.school.nz>
2024-03-13 14:21:49 +1300

    Update

--- Working with Lists/08_yahtzee_lite_v1.py
+++ Working with Lists/08_yahtzee_lite_v1.py
@@ -3,31 +3,22 @@
 import easygui
 import random
 
-number = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-          "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
-suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
-round_1 = 0
-
+dice = ["1", "2", "3", "4", "5", "6"]
+number = 0
 while True:
-    deck = []
-    for suit in suits:
-        for card in number:
-            deck.append([card, suit])
-    random.shuffle(deck)
-    draw = []
-    for card in deck[0:7]:
-        draw.append(f"Card {deck.index(card)+1}: {card[0]} of {card[1]}")
-
-    show_cards = ""
-    for item in draw:
-        show_cards = f"\n-    ".join(draw)
-    round_1 += 1
-    play_again = easygui.buttonbox(f"Round {round_1}:\nYou have drawn:\n\n"
-                                   f"-    {show_cards}\n\nWould you like to "
-                                   f"play again?", "Result",
-                                   ["Yes", "No"])
-    if play_again == "Yes":
-        continue
+    result = []
+    for i in range(5):
+        rolled = random.choice(dice)
+        result.append(rolled)
+    result.sort()
+    final_result = ", ".join(result)
+    sequence = easygui.buttonbox(f"You have rolled: {final_result}\n\n"
+                                 f"Choose:", "Result",
+                                 ["Roll again", "Stick"])
+    if sequence == "Stick" or number == 2:
+        for item in result:
+            if item == result[item]:
+                
     else:
-        break
-easygui.msgbox("Thank you for using this program", "Goodbye")
+        number += 1
+        continue
