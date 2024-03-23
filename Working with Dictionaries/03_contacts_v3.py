f8bbf0d4e0c12097c2ea14bb9c074ffb3d8f49d8
butlerdtc <butlerr2@middleton.school.nz>
2024-03-23 19:42:42 +1300

    Update 03_contacts_v3.py

--- Working with Dictionaries/03_contacts_v3.py
+++ Working with Dictionaries/03_contacts_v3.py
@@ -1,5 +1,13 @@
 import easygui
 
+
+# Formats the inputted phone number to a 000 000 0000 order
+def phone_formatter(number):
+    formatted_number = '{:010}'.format(number)
+    return (f'{formatted_number[:3]} {formatted_number[3:6]} '
+            f'{formatted_number[6:]}')
+
+
 contacts = {"1": {"First name": "Rafael", "Last name": "Aang",
                   "Mobile": "027 337 8089", "Email": "raf@gmail.com"},
             "2": {"First name": "Alex", "Last name": "Lau",
@@ -15,41 +23,57 @@ while True:
     if answer == "Add":
         while True:
             new_ID = easygui.integerbox("Enter new contacts ID: ",
-                                        "New ID")
-            if new_ID in contacts:
+                                        "New ID", upperbound=300)
+            string_new_id = f"{new_ID}"
+            if string_new_id in contacts:
                 easygui.msgbox("This ID is already assigned to a contact\n"
                                "Please enter a new ID")
                 continue
             else:
-                contacts[new_ID] = {}
+                contacts[string_new_id] = {}
                 break
 
         first_name = easygui.enterbox("Enter first name: ",
                                       "First Name")
-        contacts[new_ID]['First name'] = first_name
+        contacts[string_new_id]['First name'] = first_name.title()
 
         last_name = easygui.enterbox("Enter last name: ",
                                      "Last Name")
-        contacts[new_ID]['Last name'] = last_name
+        contacts[string_new_id]['Last name'] = last_name.title()
 
         while True:
-            mobile = easygui.integerbox("Enter mobile number: ",
-                                        "Mobile Number")
-            if mobile in contacts:
-                easygui.msgbox("Another contact already has this number")
+            count_1 = 0
+            mobile = easygui.integerbox("Enter mobile number\nMust be 10 "
+                                        "digits or less and with no spaces: ",
+                                        "Mobile Number",
+                                        upperbound=100000000000000)
+            string_mobile = phone_formatter(mobile)
+            for contact in contacts.values():
+                if 'Mobile' in contact and string_mobile == contact['Mobile']:
+                    easygui.msgbox("Another contact already has this number",
+                                   "Mobile number in use")
+                    count_1 += 1
+                    break
+            if count_1 > 0:
                 continue
             else:
-                contacts[new_ID]['Mobile'] = mobile
+                contacts[string_new_id]['Mobile'] = string_mobile
                 break
 
         while True:
+            count_2 = 0
             email = easygui.enterbox("Enter email address: ",
                                      "Email Address")
-            if email in contacts:
-                easygui.msgbox("Another contact already has this email")
+            for contact in contacts.values():
+                if 'Email' in contact and email == contact['Email']:
+                    easygui.msgbox("Another contact already has this email",
+                                   "Email in use")
+                    count_2 += 1
+                    break
+            if count_2 > 0:
                 continue
             else:
-                contacts[new_ID]['Email'] = email
+                contacts[string_new_id]['Email'] = email
                 break
 
     elif answer == "List":
